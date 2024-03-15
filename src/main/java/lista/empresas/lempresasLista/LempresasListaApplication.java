c9f8a93054512758fb5376750368ed75e3c1772e
Jadeilton <jadeiltondm@gmail.com>
2024-03-15 15:24:28 -0300

    Initial commit

---
+++ src/main/java/lista/empresas/lempresasLista/LempresasListaApplication.java
@@ -0,0 +1,13 @@
+package lista.empresas.lempresasLista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LempresasListaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LempresasListaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lista/empresas/lempresasLista/LempresasListaApplicationTests.java
@@ -0,0 +1,13 @@
+package lista.empresas.lempresasLista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LempresasListaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
